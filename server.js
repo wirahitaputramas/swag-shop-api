acde058929d31fb70375e22a754c9c5a02b633b4
wirahitaputramas <wirahitaputramas@gmail.com>
2020-08-31 22:56:57 +0800

    Adding new model (wishlist) and developing API for post request

--- server.js
+++ server.js
@@ -4,9 +4,25 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var db = mongoose.connect('mongodb://localhost/swag-shop');
 
+var Product = require('./model/product');
+var WishList = require('./model/wishlist');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({exteded: false}));
+
+app.post('/product', function(request, response){
+    var product = new Product();
+    product.title = request.body.title;
+    product.price = request.body.price;
+    product.save(function(err, savedProduct){
+        if (err) {
+            response.status(500).send({error: "Could not save product"});
+        } else {
+            response.send(savedProduct);
+        }
+    })
+})
+
 
 app.listen(3000, function() {
     console.log("Swag Shop API running on port 3000...");
